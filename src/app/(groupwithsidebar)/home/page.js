55dd33fdf92b89f74ab3769394750d0ffeee551a
Tasniem Seiam <tasniemseiam@gmail.com>
2024-09-22 13:23:48 +0300

    update style

--- src/app/(groupwithsidebar)/home/page.js
+++ src/app/(groupwithsidebar)/home/page.js
@@ -12,7 +12,7 @@ const Blog = async () => {
     <div className="container">
       <h2 className=" text-gray-600 text-3xl mt-5  border-b-2 border-gray-300 pb-2" >All Products</h2>
      
-      <div className="  flex flex-row flex-wrap gap-4 my-4 ">
+      <div className="  flex flex-row flex-wrap gap-4 my-4 mx-auto ">
         {products.map((product) => (
           <Link href={`/product/${product.id}`} key={product._id}>
             <CardProduct
